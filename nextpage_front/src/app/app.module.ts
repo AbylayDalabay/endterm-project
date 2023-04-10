b71e0a4204727a0b4f0a80a4fd9a9cbad02bff17
dilyara4949 <90037377+dilyara4949@users.noreply.github.com>
2023-04-10 12:40:49 +0600

    about-page get started

--- nextpage_front/src/app/app.module.ts
+++ nextpage_front/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
+import { AboutPageComponent } from './about-page/about-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopBarComponent
+    TopBarComponent,
+    AboutPageComponent
   ],
   imports: [
     BrowserModule,
