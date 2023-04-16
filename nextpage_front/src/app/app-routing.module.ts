560d2c58cd94e016ba550135e9e9ed5562a0604f
dilyara4949 <90037377+dilyara4949@users.noreply.github.com>
2023-04-16 17:46:13 +0600

    routerlinks added

--- nextpage_front/src/app/app-routing.module.ts
+++ nextpage_front/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { SignInPageComponent } from './sign-in-page/sign-in-page.component';
+import { SignUpPageComponent } from './sign-up-page/sign-up-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: 'home', component: HomePageComponent},
+  {path: 'about', component: AboutPageComponent},
+  {path: 'signin', component: SignInPageComponent},
+  {path: 'signup', component: SignUpPageComponent},
+  {path: '', redirectTo: 'about', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
