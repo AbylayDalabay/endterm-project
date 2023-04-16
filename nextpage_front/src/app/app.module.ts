a6e1be0c8ca52a64f217dfb5f5beb0f4d48b873f
dilyara4949 <90037377+dilyara4949@users.noreply.github.com>
2023-04-16 17:02:40 +0600

    solving conflicts

--- nextpage_front/src/app/app.module.ts
+++ nextpage_front/src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
+import { SignUpPageComponent } from './sign-up-page/sign-up-page.component';
+import { SignInPageComponent } from './sign-in-page/sign-in-page.component';
 import { AboutPageComponent } from './about-page/about-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopBarComponent,
+    SignUpPageComponent,
+    SignInPageComponent,
     AboutPageComponent
   ],
   imports: [
