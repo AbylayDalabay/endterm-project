e84b3c5b1c3d5487a9c3d32761e59a0bc889d900
ModeBeater <tashimov_nurh@mail.ru>
2023-04-26 21:44:34 +0600

    created 5 star

--- nextpage_front/src/app/book-page/book-page.component.ts
+++ nextpage_front/src/app/book-page/book-page.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./book-page.component.css']
 })
 export class BookPageComponent {
-
+    rate : number = 2;
+    ngOnInit(): void{
+      const ratingItemsList = document.querySelectorAll('.rating_item');
+      const ratingItemsArray = Array.prototype.slice.call(ratingItemsList);
+      ratingItemsArray.forEach(item => item.parentNode.dataset.totalValue = this.rate)
+    }
+    changeRate(rate:number){
+        this.rate = rate;
+        // alert(this.rate)
+        const ratingItemsList = document.querySelectorAll('.rating_item');
+        const ratingItemsArray = Array.prototype.slice.call(ratingItemsList);
+        ratingItemsArray.forEach(item => item.parentNode.dataset.totalValue = this.rate)
+        // for(let i = 1; i <= this.rate; i++){
+        //   ratingItemsArray[i]
+        // }
+    }
 }
