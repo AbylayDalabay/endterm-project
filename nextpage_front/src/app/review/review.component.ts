41cec41bb315e71b64a5a147bde7152e4d760eff
ModeBeater <tashimov_nurh@mail.ru>
2023-04-29 01:15:53 +0600

    created add review

--- nextpage_front/src/app/review/review.component.ts
+++ nextpage_front/src/app/review/review.component.ts
@@ -1,13 +1,22 @@
 import {Component, Input} from '@angular/core';
 import {Review} from "../models/review";
-
+import { reviews } from '../models/review';
 @Component({
   selector: 'app-review',
   templateUrl: './review.component.html',
   styleUrls: ['./review.component.css']
 })
 export class ReviewComponent {
-  @Input() review: string | undefined;
+  review: string | undefined;
   profile: string | undefined;
   rating: number | undefined;
+  @Input()bookId: number | undefined;
+  myList = reviews;
+  ngOnInit(): void{
+    this.myList = reviews.filter((book) => book.bookId == this.bookId);
+    // alert(this.selectedOption)
+    // const selectElement = document.getElementById('select') as HTMLSelectElement;
+    // this.selectedOption = selectElement.value;
+  }
+  mylist = reviews;
 }
