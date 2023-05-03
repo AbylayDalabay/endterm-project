2827225c661b577210c84dede17acf75b3fa2d61
ModeBeater <tashimov_nurh@mail.ru>
2023-05-03 22:56:20 +0600

    books back get post

--- nextpage_back/nextpage/api/urls.py
+++ nextpage_back/nextpage/api/urls.py
@@ -4,10 +4,11 @@ from api import views
 from rest_framework_jwt.views import obtain_jwt_token
 # noinspection PyUnresolvedReferences
 from api.views.signup import RegistrationAPIView
-
+from api.views.books import books, book_by_id
 urlpatterns = [
     # path('hell/', views.postb),
     path('login/', obtain_jwt_token), # post (token)
     path('register/', RegistrationAPIView.as_view()), # post (token)
-
+    path('books/', books),
+    path('books/<int:id>/', book_by_id)
 ]
