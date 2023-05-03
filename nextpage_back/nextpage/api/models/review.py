f8e407d18f79f15e2614f4aea9c37178a4831f17
dilyara4949 <90037377+dilyara4949@users.noreply.github.com>
2023-05-03 16:50:55 +0600

    models modified back

---
+++ nextpage_back/nextpage/api/models/review.py
@@ -0,0 +1,8 @@
+from django.db import models
+from api.models.book import Book
+
+class Review(models.Model) :
+    def __str__(self):
+        return f'{self.count}'
+    review = models.TextField()
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
