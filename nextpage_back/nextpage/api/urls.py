2a2fa07aa08fc15533c1e94ffa0ab51ddbbe1c39
dilyara4949 <90037377+dilyara4949@users.noreply.github.com>
2023-05-05 03:45:29 +0600

    crud for userlist

--- nextpage_back/nextpage/api/urls.py
+++ nextpage_back/nextpage/api/urls.py
@@ -6,17 +6,23 @@ from api.views.signup import RegistrationAPIView
 # noinspection PyUnresolvedReferences
 from api.views.books import books, book_by_id
 from api.views import category
-from api.views.userlist import UserListAPI
+from api.views import userlist
 urlpatterns = [
     path('login/', obtain_jwt_token), # post (token)
     path('register/', RegistrationAPIView.as_view()), # post (token)
     path('books/', books),
     path('books/<int:id>/', book_by_id),
 
+
     path('categories/', category.CategoryListAPIView.as_view()),
     path('categories/<int:category_id>/', category.CategoryDetailAPIView.as_view()),
     path("categories/<int:category_id>/books/", category.BooksByCategoryAPIView.as_view()),
-    path('ist/<int:user_id>/<int:list_id>/books/', UserListAPI.as_view()),
+    path('list/<int:list_id>/books/', userlist.UserListDetailAPI.as_view()),
+    path('list/<str:list_name>/books/', userlist.BookOneUserListAPI.as_view()),
+    # path('list/<int:id>/<str:list_name>/books/', userlist.BookOneUserListAPI.as_view()),
     # path('test/', userlist.create_user_wishlists)
 
+
 ]
+
+
--- nextpage_back/nextpage/api/views/userlist.py
+++ nextpage_back/nextpage/api/views/userlist.py
@@ -1,8 +1,11 @@
-from django.http import JsonResponse
+import json
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import Http404
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 from api.serializers.category import CategorySerializer2
 from api.serializers.book import BookSerializer2
 from api.models.category import Category
@@ -13,15 +16,71 @@ from api.serializers.userlist import ListSerializer2
 
 
 
-class UserListAPI(APIView):
-    
-
-
-    def get(self, request, user_id, list_id):
+class UserListDetailAPI(APIView):
+    def get(self, request,  list_id):
+        user_id = request.user
+        # return JsonResponse({user_id:99})
         userlist = UserList.objects.get(pk=list_id)
         serializer = ListSerializer2(userlist)
         return Response(serializer.data)
+    
 
+
+class BookOneUserListAPI(APIView):
+    def getListObject(self, list_name, user):
+        try:
+            return UserList.objects.get(name=list_name, user=user)
+        except User.DoesNotExist as e:
+            return Response(str(e), status=status.HTTP_400_BAD_REQUEST)
+        
+    def getBook(self, id):
+        try:
+            return Book.objects.get(pk=id)
+        except Book.DoesNotExist as e:
+            return HttpResponse(status=404)
+#     def getUser(self, user_id):
+#         try:
+#             return User.objects.get(pk=user_id)
+#         except User.DoesNotExist as e:
+#             return Response(str(e), status=status.HTTP_400_BAD_REQUEST)
+        
+
+    def get(self, request, list_name):
+        user = request.user
+        userlist = self.getListObject(list_name, user)
+        books = userlist.books.all()
+        if (books.count()==0):
+            return JsonResponse({"not":"found"})
+        else:
+                serializer = BookSerializer2(books)
+                # serializer = ListSerializer2(userlist)
+                return Response(serializer.data)
+    def post(self, request, list_name):
+        data = json.loads(request.body)
+
+        book_id = data.get('book','')
+        book = self.getBook(book_id)
+        user = request.user
+        userlist = self.getListObject(list_name, user)
+        userlist.books.add(book)
+        userlist.save()
+        serializer = ListSerializer2(userlist)
+        return Response(serializer.data)
+        # return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def delete(self, request, list_name) :
+        data = json.loads(request.body)
+        book_id = data.get('book','')
+        book = self.getBook(book_id)
+        user = request.user
+        userlist = self.getListObject(list_name, user)
+        userlist.books.remove(book)
+        serializer = ListSerializer2(userlist)
+        return Response(serializer.data)
+
+        
+
+
+    
 
         
 
